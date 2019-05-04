c2a34db180b6b4e92788eeafff982d0c186b68a5
Visharad G Dhavle <visharad.dhavle@gmail.com>
2019-05-05 00:16:23 +0530

    mediawiki: alpha - variables defined

---
+++ terraform/compute/variables.tf
@@ -0,0 +1,11 @@
+var "ami" {}
+
+var "key_name" {}
+var "public_key_path" {}
+var "web_instance_type" {}
+var "web_security_group" {}
+var "db_instance_type" {}
+var "db_security_group" {}
+var "web_subnet_a" {}
+var "web_subnet_b" {}
+var "db_subnet" {}
