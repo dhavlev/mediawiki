4687bfe26564ef5903d6e2104798d83cc7874a9c
Visharad G Dhavle <visharad.dhavle@gmail.com>
2019-05-05 01:28:27 +0530

    mediawiki: alpha - junk line removed

--- terraform/compute/main.tf
+++ terraform/compute/main.tf
@@ -86,33 +86,8 @@ resource "aws_elb" "mw_elb" {
     Project = "mediawiki"
   }
 }
-    "${aws_subnet.wp_public2_subnet.id}",
-  ]
 
-  security_groups = ["${aws_security_group.wp_public_sg.id}"]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  health_check {
-    healthy_threshold   = "${var.elb_healthy_threshold}"
-    unhealthy_threshold = "${var.elb_unhealthy_threshold}"
-    timeout             = "${var.elb_timeout}"
-    target              = "TCP:80"
-    interval            = "${var.elb_interval}"
-  }
-
-  cross_zone_load_balancing   = true
-  idle_timeout                = 400
-  connection_draining         = true
-  connection_draining_timeout = 400
-
-  tags {
-    Name = "wp_${var.domain_name}-elb"
+_name}-elb"
   }
 }
 
